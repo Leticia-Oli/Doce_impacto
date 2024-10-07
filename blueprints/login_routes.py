b4532231318bc79457059d1be5caf6a626dbbca9
Leticia Oliveira <le.0206@hotmail.com>
2024-10-07 17:18:48 -0300

    Adição do carrinho

--- blueprints/login_routes.py
+++ blueprints/login_routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, request, jsonify, render_template, redirect, url_for
+from flask import Blueprint, request, jsonify, render_template, redirect, url_for, session
 from config import db
 
 login_blueprint = Blueprint('login', __name__)
